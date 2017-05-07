43a62ac710c532462c4a42549d49eac91ae75075
maric.vid <maric.vid@gmail.com>
2017-05-07 10:26:25 -0400

    cleanup

--- inc/demolist.php
+++
@@ -1,9 +0,0 @@
-<?php
-$directory = "demos/";
-$phpfiles = glob($directory . "*.zip");
-
-foreach($phpfiles as $phpfile)
-{
-    echo '<li><a href="'.$phpfile.'">'.basename($phpfile).'</a></li>';
-}
-?>
